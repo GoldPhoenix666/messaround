000db98946c666bc1d0e5fbf5a80820c06ab2c01
DESKTOP-1IGS2AN\Niall <niall@food-surveys.com>
2017-10-11 15:35:12 +0100

    fixed #5, removed unneeded links

--- correct.php
+++ correct.php
@@ -240,9 +240,6 @@ $table3 .= "
 
 <head>
 
-	<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
-  	<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
-  	<!--script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
   	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
   	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
