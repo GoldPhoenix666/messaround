368ed465f7efede89bc55a55f74e50026d7a8170
DESKTOP-1IGS2AN\Niall <niall@food-surveys.com>
2017-10-12 11:36:52 +0100

    altered indentation again, got rid of useless comments, changed variable names in both js and php

--- correct.php
+++ correct.php
@@ -1,215 +1,150 @@
 <?php
-
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "piechart";
 
-
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
+
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+    die("Connection failed: " . $conn->connect_error);} 
 
-
-//THIS IS CODE FOR THE PIECHART//START
 $piechartquery = mysqli_query($conn, "SELECT *, (SUM(`hours`)* 100 / (SELECT SUM(hours) FROM `pieinfo`)) AS `percent` FROM `pieinfo` GROUP BY `dataid`");
 
 $add_rows = '';
 while ($pierow = mysqli_fetch_assoc($piechartquery)) {
-    $add_rows .= 'data.addRow(["' . $pierow['activity'] . "  " . $pierow['hours'] . " hours " .
-    number_format((float)$pierow['percent'], 2, '.', '')  . "%" . '", ' . $pierow['hours'] . ']);';
-};
-//THIS IS CODE FOR THE PIECHART//END
+    $add_rows .= 'piedata.addRow(["' . $pierow['activity'] . "  " . $pierow['hours'] . " hours " .
+    number_format((float)$pierow['percent'], 2, '.', '')  . "%" . '", ' . $pierow['hours'] . ']);';};
 
-//break bweteen
-
-//This is the code for the BARCHART//START
 $barchartquery = mysqli_query($conn, "SELECT * FROM `pieinfo`");
 
 $add_rows2 = '';
 while ($barrow = mysqli_fetch_assoc($barchartquery)) {
-    $add_rows2 .= '
-    data.addRow(["' . $barrow['activity'] . "  " . '", ' . $barrow['hours'] . ']);';
-};
-//This is the code for the BARCHART//END
+    $add_rows2 .= 'bardata.addRow(["' . $barrow['activity'] . "  " . '", ' . $barrow['hours'] . ']);';};
 
-//break bweteen
-
-//Query Status//START
-//If the GET command 'status1' is in the URL display this message
+//If the GET command is in the URL display this message
 if(!empty($_GET['status1'])) {
 	    $message = mysqli_real_escape_string($conn, $_GET['status1']);
-$statusmessage = "<h1 class=\"alert alert-success alert-dismissable fade in\"><a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">×</a>Update Successful!<p style=\"text-decoration:underline; font-size:20px;\">Information has been updated</p></h1>";
-}
+$statusmessage = '<h1 class="alert alert-success alert-dismissable fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>Update Successful!<p style="text-decoration:underline; font-size:20px;">Information has been updated</p></h1>';}
 
-//If the GET command 'status2' is in the URL display this message
 if(!empty($_GET['status2'])) {
     $message = mysqli_real_escape_string($conn, $_GET['status2']);
-$statusmessage = "<h1 class=\"alert alert-success alert-dismissable fade in\"><a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">×</a>New Activity Added!<p style=\"text-decoration:underline; font-size:20px;\">Information has been added</p></h1>";
-}
+$statusmessage = '<h1 class="alert alert-success alert-dismissable fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>New Activity Added!<p style="text-decoration:underline; font-size:20px;">Information has been added</p></h1>';}
 
-//If the GET command 'status3' is in the URL display this message
 if(!empty($_GET['status3'])) {
     $message = mysqli_real_escape_string($conn, $_GET['status3']);
-$statusmessage =  "<h1 class=\"alert alert-success alert-dismissable fade in\"><a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">×</a>Record Deleted!<p style=\"text-decoration:underline; font-size:20px;\">Information has been deleted</p></h1>";
-}
+$statusmessage =  '<h1 class="alert alert-success alert-dismissable fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>Record Deleted!<p style="text-decoration:underline; font-size:20px;">Information has been deleted</p></h1>';}
 
-//If the GET command 'status4' is in the URL display this message
 if(!empty($_GET['status4'])) {
     $message = mysqli_real_escape_string($conn, $_GET['status4']);
-$statusmessage =  "<h1 class=\"alert alert-danger alert-dismissable fade in\"><a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">×</a>Update Unsuccessful!<p style=\"text-decoration:underline; font-size:20px;\">Information has not been updated</p></h1>";
-}
+$statusmessage =  '<h1 class="alert alert-danger alert-dismissable fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>Update Unsuccessful!<p style="text-decoration:underline; font-size:20px;">Information has not been updated</p></h1>';}
 
-//If the GET command 'status5' is in the URL display this message
 if(!empty($_GET['status5'])) {
     $message = mysqli_real_escape_string($conn, $_GET['status5']);
-$statusmessage =  "<h1 class=\"alert alert-danger alert-dismissable fade in\"><a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">×</a>Entry Not Added!<p style=\"text-decoration:underline; font-size:20px;\">Information has not been added</p></h1>";
-}
+$statusmessage =  '<h1 class="alert alert-danger alert-dismissable fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>Entry Not Added!<p style="text-decoration:underline; font-size:20px;">Information has not been added</p></h1>';}
 
-//If the GET command 'status6' is in the URL display this message
 if(!empty($_GET['status6'])) {
     $message = mysqli_real_escape_string($conn, $_GET['status6']);
-$statusmessage =  "<h1 class=\"alert alert-danger alert-dismissable fade in\"><a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">×</a>Infomation Unaltered!<p style=\"text-decoration:underline; font-size:20px;\">Information has not been altered</p></h1>";
-}
-//Query Status//END
-
-//break between
+$statusmessage =  '<h1 class="alert alert-danger alert-dismissable fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>Infomation Unaltered!<p style="text-decoration:underline; font-size:20px;">Information has not been altered</p></h1>';}
 
 //Data Grab for the first table//START
 $datagrab = mysqli_query($conn, "SELECT * FROM `pieinfo` LEFT JOIN `person` ON `pieinfo`.`personid` = `person`.`personid` ORDER BY `personname` ASC, `activity` ASC");
 
-$acttable = "
-<div class=\"row-fluid\">
-	<div class=\"span4\" style=\"border:0px blue solid;\" >
-		<table class=\"table-hover align-self-start \" style=\"float: none; margin: 0 auto;\">
+$activitytable = '
+<div class="row-fluid">
+	<div class="span4" style="border:0px blue solid;" >
+		<table class="table-hover align-self-start " style="float: none; margin: 0 auto;">
 			<tr>
-				<th>Name</th>
-				<th>Hours</th>
-				<th>Minutes</th>
-				<th>Activity</th>
-				<th>Delete</th>
-				<th>Edit</th>
-			</tr>
-";
+				<th>Name</th><th>Hours</th><th>Minutes</th><th>Activity</th><th>Delete</th><th>Edit</th>
+			</tr>';
 
 while ($row = mysqli_fetch_assoc($datagrab)) {
-$acttable .=  "
+$activitytable .=  '
 <tr>
-	<td>" .$row['personname']. "</td>
-	<td>" .$row['hours']. "</td>
-	<td>" .$row['minutes']. "</td>
-	<td>" .$row['activity']. "</td>
+	<td>' .$row['personname']. '</td>
+	<td>' .$row['hours']. '</td>
+	<td>' .$row['minutes']. '</td>
+	<td>' .$row['activity']. '</td>
 	<td>
-		<form action=\"delete.php\" method=\"post\">
-			<input type=\"hidden\" name=\"dataid\" value=\"". $row['dataid'] ."\" />
-			<input type=\"submit\" style=\"margin-top:20px;\" class=\"btn btn-default\" value=\"Delete\" name=\"delete\" />
+		<form action="delete.php" method="post">
+			<input type="hidden" name="dataid" value="'. $row['dataid'] .'" />
+			<input type="submit" style="margin-top:20px;" class="btn btn-default" value="Delete" name="delete" />
 		</form>
 	</td>
 	<td>
-		<form action=\"update.php\" method=\"post\">
-			<input type=\"hidden\" name=\"dataid\" value=\"". $row['dataid'] ."\" />
-			<input type=\"submit\"  style=\"margin-top:20px;\" class=\"btn btn-default\" value=\"Update\" name=\"update\" />
+		<form action="update.php" method="post">
+			<input type="hidden" name="dataid" value="'. $row['dataid'] .'" />
+			<input type="submit"  style="margin-top:20px;" class="btn btn-default" value="Update" name="update" />
 		</form>
 	</td>
-</tr>";
+</tr>';
 }
 
-$acttable .= "
-</table>
-</div>
-";
-//Data Grab for the first table//END
-
-//break between tables
+$activitytable .= '</table></div>';
 
 //Data Grab for the second table//START
 $conclusion = mysqli_query($conn, "SELECT `person`.`personid`, `personname`, IFNULL(SUM(`hours`), 0) as 'hours', IFNULL(SUM(`minutes`), 0) as 'minutes', IFNULL(COUNT(`activity`), 0) as 'activity' FROM `person` LEFT JOIN `pieinfo` ON `person`.`personid` = `pieinfo`.`personid` GROUP BY `personname` ORDER BY `activity` DESC ");
 
 $addsecbar = "";
 
-$totaltable = "
-<div class=\"span4\" style=\"border:0px orange solid;\" >
-	<table class=\"table-hover\" style=\"float: none; margin: 0 auto;\">
+$totaltable = '
+<div class="span4" style="border:0px orange solid;" >
+	<table class="table-hover" style="float: none; margin: 0 auto;">
 		<tr>
-			<th>Name</th>
-			<th>Total Hours</th>
-			<th>Total Minutes</th>
-			<th># of Activities</th>
-			<th>Delete</th>
-		</tr>
-";
+			<th>Name</th><th>Total Hours</th><th>Total Minutes</th><th># of Activities</th><th>Delete</th>
+		</tr>';
 
 while ($row = mysqli_fetch_assoc($conclusion)) {
 
-$totaltable .=  "
+$totaltable .=  '
 <tr>
-	<td>" .$row['personname']. "</td>
-	<td>" .$row['hours']. "</td>
-	<td>" .$row['minutes']. "</td>
-	<td>" .$row['activity']. "</td>
+	<td>' .$row['personname']. '</td>
+	<td>' .$row['hours']. '</td>
+	<td>' .$row['minutes']. '</td>
+	<td>' .$row['activity']. '</td>
 	<td>
-		<form action=\"delete2.php\" method=\"post\">
-			<input type=\"hidden\" name=\"personid\" value=\"". $row['personid'] ."\" />
-			<input type=\"submit\" style=\"margin-top:20px;\" class=\"btn btn-default\" value=\"Delete\" name=\"delete\" />
+		<form action="delete2.php" method="post">
+			<input type="hidden" name="personid" value="'. $row['personid'] .'" />
+			<input type="submit" style="margin-top:20px;" class="btn btn-default" value="Delete" name="delete" />
 		</form>
 	</td>
-</tr>";
+</tr>';
 
 $addsecbar .= '
-data.addRow(["' . $row['personname'] . "  " . '", ' . $row['activity'] . ']);
-';	
-}
+data.addRow(["' . $row['personname'] . "  " . '", ' . $row['activity'] . ']);';}
 
-$totaltable .= "
-</table>
-</div>
-";
-//Data Grab for the second table//END
-
-//break between tables
+$totaltable .= '</table></div>';
 
 //Data Grab for the third table//START
-$threecharm = mysqli_query($conn, "SELECT `activity`, COUNT(`activity`) AS MOST_FREQUENT FROM `pieinfo` GROUP BY `activity` ORDER BY COUNT(`activity`) DESC");
+$occurrencedata = mysqli_query($conn, "SELECT `activity`, COUNT(`activity`) AS MOST_FREQUENT FROM `pieinfo` GROUP BY `activity` ORDER BY COUNT(`activity`) DESC");
 
-$occtable = "
-<div class=\"span4\" style=\"border:0px green solid;\" >
-	<table class=\"table-hover\" style=\"float: none; margin: 0 auto;\" >
+$occurrencetable = '
+<div class="span4" style="border:0px green solid;" >
+	<table class="table-hover" style="float: none; margin: 0 auto;" >
 		<tr>
 			<th>Activity</th>
 			<th>Occurrence</th>
-		</tr>
-";
+		</tr>';
 
-while ($lowrow = mysqli_fetch_assoc($threecharm)) {
-$occtable .= "
+while ($lowrow = mysqli_fetch_assoc($occurrencedata)) {
+$occurrencetable .= '
 <tr>
-	<td>" .$lowrow['activity']. "</td>
-	<td>" .$lowrow['MOST_FREQUENT']. "</td>
-</tr>
-";}
+	<td>' .$lowrow['activity']. '</td>
+	<td>' .$lowrow['MOST_FREQUENT']. '</td>
+</tr>';}
 
-$occtable .= "
-</table>
-</div>
-</div>
-";
-//Data Grab for the third table//END
+$occurrencetable .= "</table></div></div>";
 ?>
 <!DOCTYPE html>
 
 <html>
 
 <head>
-
   	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
   	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
-
 	<style type="text/css">
 
 		body{
@@ -224,31 +159,24 @@ $occtable .= "
 
 	</style>
 
-
 	<script type="text/javascript" >
       	google.charts.load('current', {'packages':['corechart']});
       	google.charts.setOnLoadCallback(drawChart);
 
       	function drawChart() {
 
-     	var data = new google.visualization.DataTable();
+     	var piedata = new google.visualization.DataTable();
 
-      	data.addColumn('string', 'time');
-      	data.addColumn('number', 'hours');
+      	piedata.addColumn('string', 'time');
+      	piedata.addColumn('number', 'hours');
 
-        <?php echo $add_rows ?>
+<?php echo $add_rows ?>
 
-      	var options = {
-        title: 'List of all Infomation',
-        sliceVisibilityThreshold: 0
-     	 };
+      	var options = {title: 'List of all Infomation', sliceVisibilityThreshold: 0};
 
       	var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-      	chart.draw(data, options);
-      	// Changing legend  
-	};
+      	chart.draw(piedata, options);};
 	</script>
-
 
 	<script type="text/javascript">
   
@@ -256,41 +184,23 @@ $occtable .= "
 
 		function drawVisualization() {
 
-     	var data = new google.visualization.DataTable();
-        data.addColumn('string', 'name');
-        data.addColumn('number', 'Quantity');
+     	var bardata = new google.visualization.DataTable();
+        bardata.addColumn('string', 'name');
+        bardata.addColumn('number', 'Quantity');
 
 <?php echo $add_rows2 ?>  
 
-		data.sort({column: 1, desc: false});
+		bardata.sort({column: 1, desc: false});
 		
-      	var options = {
-        title: 'Activities in hours',
-        chartArea: {width: '60%',},
-        hAxis: {
-        chartArea: {height: '70%',},
-        title: 'Hours',
-        minValue: 0
-        },
+      	var options = {title: 'Activities in hours', chartArea: {width: '60%',},
+      	hAxis: {chartArea: {height: '70%',}, title: 'Hours', minValue: 0},
 
-        vAxis: {
-        title: 'Activity',
-        direction:'1'
+        vAxis: {title: 'Activity', direction:'1'},
 
-        },
-
-        annotation:{
-        1:{
-
-        style:'line'
-
-          }}};
+        annotation:{1:{style:'line'}}};
       	var chart = new google.visualization.BarChart(document.getElementById('chart_div2'));
-      	chart.draw(data, options);
-
-    };
+      	chart.draw(bardata, options);};
 	</script>
-
 
 	<script type="text/javascript">
   
@@ -298,103 +208,61 @@ $occtable .= "
 
 		function drawVisualization() {
 
-     	var data = new google.visualization.DataTable();
-        data.addColumn('string', 'name');
-        data.addColumn('number', 'Quantity');
+     	var namechartdata = new google.visualization.DataTable();
+        namechartdata.addColumn('string', 'name');
+        namechartdata.addColumn('number', 'Quantity');
     
 <?php echo $addsecbar ?>  
 
-      	var options = {
-        title: 'Peoples activities',
-        chartArea: {width: '60%',},
-        hAxis: {
-        chartArea: {height: '70%',},
-        title: '# of Activities',
-        minValue: 0
-        },
+      	var options = {title: 'Peoples activities', chartArea: {width: '60%',},
+      	hAxis: {chartArea: {height: '70%',}, title: '# of Activities', minValue: 0},
 
-        vAxis: {
-        title: 'Name',
-        direction:'-1'
-        },
+        vAxis: {title: 'Name', direction:'-1'},
 
-        annotation:{
-        1:{
-
-        style:'line'
-
-          }}};
+        annotation:{1:{style:'line'}}};
       	var chart = new google.visualization.BarChart(document.getElementById('chart_div3'));
-      	chart.draw(data, options);
-    };
+      	chart.draw(namechartdata, options);};
 	</script>
 
 </head>
 
 <body>
-
 <!--This is to display the message after a query has been processed, also it says that if the variable is empty do not echo. By merging all of the variables into one and echoing it, it only display which one has data and does not display error messages saying the variable is empty-->
 <?php
-
 if(!empty($statusmessage)) {
-
-echo $statusmessage;
-}
+echo $statusmessage;}
 ?>
 
 <h2 style="text-align:center; text-decoration:underline;" >This is the information from the server</h2>
-
 <div class="row-fluid">
-
 	<div class="span4" style="border:0px purple solid;" >
-
 		<div id="chart_div2" style="height:500px;"></div>
-
 	</div>
-
 	<div class="span4" style="border:0px black solid;" >
-
-		<div id="chart_div3" style="height:500px;"></div>
-		
+		<div id="chart_div3" style="height:500px;"></div>		
 	</div>
-
-	<div class="span4" style="border:0px grey solid;" >
-				
+	<div class="span4" style="border:0px grey solid;" >				
 		<div id="chart_div" style="height:500px;"></div>
-
 	</div>
-
 </div>
 
-<?php echo $acttable ?>	
-
+<?php echo $activitytable ?>	
 <?php echo $totaltable ?>
-
-<?php echo $occtable ?>
+<?php echo $occurrencetable ?>
 
 <br />
 
 <div class="row-fluid">
 <div class="span6" style="border: 0px red solid; text-align: center;">
-
 	<h3>Enter Information here to add Information</h3>
-
 	<form action="newentry.php" method="post" style="float: none; margin: 0 auto;" >
-
 		<label for="Activity">Enter your activity:</label>
-
 			<input type="text" name="activity" placeholder="Activities" value="<?php echo $row['activity']; ?>">
-
 		<label for="Hours">Hours of said activity:</label>
-
 			<input type="number" name="hours" placeholder="Hours" value="<?php echo $row['hours']; ?>">
-
 		<label for="Minutes">Minutes of said activity:</label>
-
 			<input type="number" name="minutes" min="0" max="59" placeholder="Minutes" value="<?php echo $row['minutes']; ?>">
-
 		<label for="personname">Pick your name:</label>
-
 			<select name="personid">
 
 				<?php
@@ -405,37 +273,22 @@ echo $statusmessage;
 				};?>
 
  			</select>
-
 			<br />
-
  			<br />
-
 		<button type="submit" style="margin-top:-10px;" class="btn btn-success btn-small">Add</button>
-
 	</form>
-
 </div>
 
 <div class="span6" style="border: 0px red solid;">
-
 	<form action="addname.php" method="post" style="float: none; margin: 0 auto; text-align: center;">
-
-		<h3>Use this form to add a new name</h3>
-		
+		<h3>Use this form to add a new name</h3>		
 		<label for="Newname" >Add a new user:</label>
-
 			<input type="text" name="personname" placeholder="Name" value="<?php echo $row['personname'] ?>" />
-
 			<br />
-
 			<br />
-
  		<button type="submit" style="margin-top:-10px;" class="btn btn-success btn-small">Add</button>
-
 		<br />
-
 	</form>
-
 </div>
 </div>
 
